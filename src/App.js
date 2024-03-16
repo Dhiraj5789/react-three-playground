eaf7dff38f00df8d2e96d67e5daa2ce6de207d39
Dhiraj Suthar <dhirajsuthar5789@gmail.com>
2024-03-17 01:24:00 +0530

    galactic stars

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import * as THREE from "three";
+import { useRef, useState, useMemo, useEffect, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  Points,
+  PointMaterial,
+  Billboard,
+  Text,
+  TrackballControls,
+} from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
+import { generate } from "random-words";
+
+export default function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Stars />
+      </Canvas>
+    </>
+  );
+}
+
+function Stars(props) {
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 0.5 })
+  );
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled={false}
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#ffa0e0"
+          size={0.005}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+}
